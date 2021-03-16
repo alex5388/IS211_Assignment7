3705808f8b5410452565c8b23c6224669a91a212
alex5388 <alexmiller5388@gmail.com>
2021-03-16 18:45:52 -0400

    Add files via upload

--- pig.py
+++ pig.py
@@ -2,17 +2,19 @@ import random
 
 
 class Player():
+
+    round = False
     def __init__(self):
         self.score = 0
         self.turn_score = 0
-        self.round_end = 0
+
 
     def roll(self):
         self.seed = random.randint(1, 6)
         return self.seed
 
     def hold_or_roll(self):
-        while self.round_end != 1:
+        while self.round != True:
 
             choose = input("Press 'r' for roll and 'h' to hold.")
             if choose == 'r':
@@ -20,8 +22,12 @@ class Player():
                 print("You rolled a {}".format(self.seed))
                 if self.seed == 1:
                     print("Your turn is over")
-                    self.round_end = 1
-                print("Your turn score is {}".format(self.turn_score))
+                    self.round = True
+                    self.turn_score = 0
+                print("Your turn score is {}.".format(self.turn_score))
+                if self.turn_score >= 100:
+                    print("You won the Game!")
+                    self.round = True
 
 
             elif choose == 'h':
@@ -29,24 +35,18 @@ class Player():
                 print(self.score)
                 print("You received {} points toward your overall score of {}".format(self.turn_score, self.score))
                 self.turn_score = 0
-                self.round_end = 1
+                self.round_end = True
 
 
-class Game():
-
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
 
 
 
 def main():
     player1 = Player()
     player2 = Player()
-    Game(player1, player2)
-    while (player1.score < 100) or (player2.score < 100):
-        player1.hold_or_roll()
-        player2.hold_or_roll()
+    player1.hold_or_roll()
+    player2.hold_or_roll()
+
 
 
 
@@ -54,6 +54,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
