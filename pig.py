da67c1b31d4bd4c5d6746a91a438e3e865610c96
alex5388 <alexmiller5388@gmail.com>
2021-03-16 15:24:48 -0400

    Add files via upload

--- pig.py
+++ pig.py
@@ -2,33 +2,29 @@ import random
 
 
 class Player():
-
-
-    turn = True
-
     def __init__(self):
-        self.seed = 0
         self.score = 0
-
-
+        self.turn_score = 0
+        self.die_num = 0
 
     def roll(self):
         self.seed = random.randint(1, 6)
         return self.seed
 
     def hold_or_roll(self):
-        while Player.turn == True:
+        while self.die_num != 1:
+
             choose = input("Press 'r' for roll and 'h' to hold.")
             if choose == 'r':
-
-                print("Rolling...")
-                print(self.score)
-                turn_score =+ self.roll()
-                print(turn_score)
-
+                self.turn_score = self.turn_score + self.roll()
+                print("You rolled a {}".format(self.seed))
+                print("Your turn score is {}".format(self.turn_score))
 
             elif choose == 'h':
-                print("You received {} points".format(Player.turn_score))
+                self.score = self.score + self.turn_score
+                print(self.score)
+                print("You received {} points toward your overall score of {}".format(self.turn_score, self.score))
+        return self.turn_score == 0
 
 
 class Game():
@@ -36,8 +32,8 @@ class Game():
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
-        self.player1.score = 0
-        self.player2.score = 0
+        #self.player1.score = 0
+        #self.player2.score = 0
 
 
 def main():
@@ -48,5 +44,10 @@ def main():
 
 
 
+
+
 if __name__ == "__main__":
     main()
+
+
+
