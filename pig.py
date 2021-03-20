7f136f8e43dff39f772bb6e2edbaecc5178b3662
alex5388 <alexmiller5388@gmail.com>
2021-03-20 14:13:53 -0400

    Add files via upload

--- pig.py
+++ pig.py
@@ -1,56 +1,85 @@
 import random
 
 
-class Player():
-
-    round = False
+class Die():
     def __init__(self):
-        self.score = 0
-        self.turn_score = 0
-
+        self.die_number = 0
 
     def roll(self):
-        self.seed = random.randint(1, 6)
-        return self.seed
+        self.die_number = random.randint(1,6)
 
-    def hold_or_roll(self):
-        while self.round != True:
-
-            choose = input("Press 'r' for roll and 'h' to hold.")
-            if choose == 'r':
-                self.turn_score = self.turn_score + self.roll()
-                print("You rolled a {}".format(self.seed))
-                if self.seed == 1:
-                    print("Your turn is over")
-                    self.round = True
-                    self.turn_score = 0
-                print("Your turn score is {}.".format(self.turn_score))
-                if self.turn_score >= 100:
-                    print("You won the Game!")
-                    self.round = True
+    def get_die_number(self):
+        return self.die_number
 
 
-            elif choose == 'h':
-                self.score = self.score + self.turn_score
-                print(self.score)
-                print("You received {} points toward your overall score of {}".format(self.turn_score, self.score))
-                self.turn_score = 0
-                self.round_end = True
+class Player():
 
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.turn_score = 0
+        self.turn = True
+
+
+    def addtoFinal(self):
+        self.score = self.score + self.turn_score
+
+    def addtoTurn(self, points):
+        self.turn_score = self.turn_score + points
+
+    def reset(self):
+        self.turn_score = 0
+
+    def reset_turn(self):
+        self.turn = True
+
+    def hold_or_roll(self, die):
+            while self.turn:
+                choose = input("Pick 'r' to roll the die or 'h' to hold.")
+                if choose == 'r':
+                    die.roll()
+                    if die.get_die_number() == 1:
+                        #self.reset()
+                        print("----{} rolled a {}.".format(self.name, die.get_die_number()))
+                        print("You lost your TURN-SCORE points. Your OVERALL SCORE is {}. It's now the next player's turn...".format(self.score))
+                        self.turn = False
+                        self.reset()
+                    elif die.get_die_number() > 1:
+                        self.addtoTurn(die.get_die_number())
+                        print("----{} rolled a {} and your TURN-SCORE is {}.".format(self.name, die.get_die_number(), self.turn_score))
+
+                elif choose == 'h':
+                    self.addtoFinal()
+                    print("{} chose to hold, your OVERALL SCORE is currently {}. It's now the next player's turn...".format(self.name, self.score))
+                    if self.score >= 100:
+                        print("YOU'VE WON!!! game over...")
+                        exit()
+                    self.reset()
+                    self.turn = False
 
 
 
 
 def main():
-    player1 = Player()
-    player2 = Player()
-    player1.hold_or_roll()
-    player2.hold_or_roll()
+
+    die = Die()
+    player1 = Player('Player 1')
+    player2 = Player('Player 2')
+
+    game = True
+
+
+    while game:
+        player1.hold_or_roll(die)
+        player1.reset_turn()
+        player2.hold_or_roll(die)
+        player2.reset_turn()
 
 
 
 
-
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
+
+
+
